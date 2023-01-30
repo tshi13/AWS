9153ab71d44092f1fbb4b948673754c62f32e148
tshi13 <tshi13@jh.edu>
2023-01-29 22:16:20 -0500

    show images

--- src/App.js
+++ src/App.js
@@ -95,23 +95,36 @@ async function createNote(event) {
       </View>
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
+			{notes.map((note) => (
+				<Flex
+					key={note.id || note.name}
+					direction="row"
+					justifyContent="center"
+					alignItems="center"
+				>
+					<Text as="strong" fontWeight={700}>
+						{note.name}
+					</Text>
+					<Text as="span">{note.description}</Text>
+					{note.image && (
+						<Image
+							src={note.image}
+							alt={`visual aid for ${notes.name}`}
+							style={{ width: 400 }}
+						/>
+					)}
+					<Button variation="link" onClick={() => deleteNote(note)}>
+						Delete note
+					</Button>
+				</Flex>
+			))}
       </View>
+			<View
+			name="image"
+			as="input"
+			type="file"
+			style={{ alignSelf: "end" }}
+			/>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
