3dc403f07f1a02626b99a9e9fbc28d5c3dd4f5a4
tshi13 <tshi13@jh.edu>
2023-01-29 22:44:36 -0500

    fix bug

--- src/App.js
+++ src/App.js
@@ -25,47 +25,65 @@ const App = ({ signOut }) => {
     fetchNotes();
   }, []);
 
-  async function fetchNotes() {
-  const apiData = await API.graphql({ query: listNotes });
-  const notesFromAPI = apiData.data.listNotes.items;
-  await Promise.all(
-    notesFromAPI.map(async (note) => {
-      if (note.image) {
-        const url = await Storage.get(note.name);
-        note.image = url;
-      }
-      return note;
-    })
-  );
-  setNotes(notesFromAPI);
-}
+	async function fetchNotes() {
+		const apiData = await API.graphql({ query: listNotes });
+		const notesFromAPI = apiData.data.listNotes.items;
+		await Promise.all(
+			notesFromAPI.map(async (note) => {
+				if (note.image) {
+					const url = await Storage.get(note.name);
+					note.image = url;
+				}
+				return note;
+			})
+		);
+		setNotes(notesFromAPI);
+	}
 
-async function createNote(event) {
-  event.preventDefault();
-  const form = new FormData(event.target);
-  const image = form.get("image");
-  const data = {
-    name: form.get("name"),
-    description: form.get("description"),
-    image: image.name,
-  };
-  if (!!data.image) await Storage.put(data.name, image);
-  await API.graphql({
-    query: createNoteMutation,
-    variables: { input: data },
-  });
-  fetchNotes();
-  event.target.reset();
-}
+  // async function createNote(event) {
+  //   event.preventDefault();
+  //   const form = new FormData(event.target);
+  //   const data = {
+  //     name: form.get("name"),
+  //     description: form.get("description"),
+  //   };
+  //   await API.graphql({
+  //     query: createNoteMutation,
+  //     variables: { input: data },
+  //   });
+  //   fetchNotes();
+  //   event.target.reset();
+  // }
 
-  async function deleteNote({ id }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await API.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
-  }
+	async function createNote(event) {
+		event.preventDefault();
+		const form = new FormData(event.target);
+		const image = form.get("image");
+		console.log(form.get("name"));
+		console.log(image);
+		const data = {
+			name: form.get("name"),
+			description: form.get("description"),
+			image: image.name,
+		};
+		if (data.image) await Storage.put(data.name, image);
+		await API.graphql({
+			query: createNoteMutation,
+			variables: { input: data },
+		});
+		fetchNotes();
+		event.target.reset();
+	}
+
+  async function deleteNote({ id, name }) {
+		const newNotes = notes.filter((note) => note.id !== id);
+		setNotes(newNotes);
+		await Storage.remove(name);
+		await API.graphql({
+			query: deleteNoteMutation,
+			variables: { input: { id } },
+		});
+	}
 
   return (
     <View className="App">
@@ -88,6 +106,12 @@ async function createNote(event) {
             variation="quiet"
             required
           />
+					<View
+						name="image"
+						as="input"
+						type="file"
+						style={{ alignSelf: "end" }}
+					/>
           <Button type="submit" variation="primary">
             Create Note
           </Button>
@@ -96,39 +120,32 @@ async function createNote(event) {
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
 			{notes.map((note) => (
-				<Flex
-					key={note.id || note.name}
-					direction="row"
-					justifyContent="center"
-					alignItems="center"
-				>
-					<Text as="strong" fontWeight={700}>
-						{note.name}
-					</Text>
-					<Text as="span">{note.description}</Text>
-					{note.image && (
-						<Image
-							src={note.image}
-							alt={`visual aid for ${notes.name}`}
-							style={{ width: 400 }}
-						/>
-					)}
-					<Button variation="link" onClick={() => deleteNote(note)}>
-						Delete note
-					</Button>
-				</Flex>
-			))}
+  <Flex
+    key={note.id || note.name}
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Text as="strong" fontWeight={700}>
+      {note.name}
+    </Text>
+    <Text as="span">{note.description}</Text>
+    {note.image && (
+      <Image
+        src={note.image}
+        alt={`visual aid for ${notes.name}`}
+        style={{ width: 400 }}
+      />
+    )}
+    <Button variation="link" onClick={() => deleteNote(note)}>
+      Delete note
+    </Button>
+  </Flex>
+))}
       </View>
-			<View
-			name="image"
-			as="input"
-			type="file"
-			style={{ alignSelf: "end" }}
-			/>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
 };
 
 export default withAuthenticator(App);
-
