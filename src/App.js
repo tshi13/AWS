97e847c5742510345e07496e8a5a767ce4249dd5
tshi13 <tshi13@jh.edu>
2023-01-29 21:56:59 -0500

    fetch images

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { API, Storage } from 'aws-amplify';
 import {
   Button,
   Flex,
   Heading,
+  Image,
   Text,
   TextField,
   View,
   withAuthenticator,
-} from "@aws-amplify/ui-react";
+} from '@aws-amplify/ui-react';
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
@@ -25,10 +26,19 @@ const App = ({ signOut }) => {
   }, []);
 
   async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    setNotes(notesFromAPI);
-  }
+		const apiData = await API.graphql({ query: listNotes });
+		const notesFromAPI = apiData.data.listNotes.items;
+		await Promise.all(
+			notesFromAPI.map(async (note) => {
+				if (note.image) {
+					const url = await Storage.get(note.name);
+					note.image = url;
+				}
+				return note;
+			})
+		);
+		setNotes(notesFromAPI);
+	}
 
   async function createNote(event) {
     event.preventDefault();
